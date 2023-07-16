d91d3fb36f1cb2664109fdfc5f5f3856f6d05ccb
asmaa-tek <web.design4solution@gmail.com>
2023-07-16 21:19:52 +0200

    task completed

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ImageGreat(getString(R.string.all_tasks_completed), getString(R.string.nice_work))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Great(senone: String, sentwo:  String, modifier: Modifier = Modifier) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+
+    )
+    {
+        Text(
+            text = senone,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .padding(bottom = 8.dp),
+        )
+
+        Text(
+            text = sentwo,
+            fontSize = 16.sp,
+            modifier = modifier
+
+                )
+
+    }
+
+}
+
+@Composable
+fun ImageGreat(senone: String, sentwo:  String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+
+        Box(
+           contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = modifier
+            )
+            Box(
+                modifier = Modifier
+                    .padding(top = 300.dp),
+            ) {
+                Great(
+                    senone = senone,
+                    sentwo = sentwo,
+
+                    )
+
+            }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaskManagerTheme {
+        ImageGreat(stringResource(R.string.all_tasks_completed), stringResource(R.string.nice_work))
+    }
+}
